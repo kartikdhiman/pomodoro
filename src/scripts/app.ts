a53eaccc6e619e19f217785fd7a3281203c1e2ac
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2021-01-23 14:10:56 +0530

    partial: #2 create pomodoro timer

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,1 +1,15 @@
 import '../styles/app.scss';
+
+
+const START = 'START';
+const PAUSE = 'PAUSE';
+const RESUME = 'RESUME';
+
+const elTimer = document.getElementById('timer')!;
+const btnControl = document.getElementById('control')!;
+
+
+
+function initialize() {
+    btnControl.textContent = START;
+}
